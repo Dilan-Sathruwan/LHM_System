112c7668624ed9d7055efef902dc86f7ccf56aec
Ravindu lasal <ravindusl01@gmail.com>
2024-09-19 00:39:19 +0530

    update lecture hall page

--- admin/LectureHall.php
+++ admin/LectureHall.php
@@ -7,15 +7,88 @@
     <!-- Navbar End -->
 
 
-    <!-- Blank Start -->
+    <!-- data Subject table Start -->
     <div class="container-fluid pt-4 px-4">
-        <div class="row vh-100 bg-dark rounded align-items-center justify-content-center mx-0">
-            <div class="col-md-6 text-center">
-                <h3>This is blank page</h3>
+        <div class="bg-light text-center rounded p-4">
+            <div class="d-flex align-items-center justify-content-between mb-4">
+                <h3 class="mb-0">Lecture Halls</h3>
+                <a class="btn btn-sm btn-primary" href="">Add Halls</a>
+            </div>
+            <div class="table-responsive">
+                <table class="table text-start align-middle table-bordered table-hover mb-0">
+                    <thead>
+                        <tr class="text-dark">
+                            <th scope="col">Hall Number</th>
+                            <th scope="col">Hall Name</th>
+                            <th scope="col">Student capacity</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>01 Jan 2045</td>
+                            <td>INV-0123</td>
+                            <td>Jhon Doe</td>
+                            <td>$123</td>
+                            <td class="d-flex align-items-lg-center justify-content-around">
+                                <a href="" class=""><i class="fas fa-eye fa-lg "></i></a>
+                                <a href="" class=""><i class="fas fa-user-edit fa-lg"></i></a>
+                                <a href="" class=""><i class="fas fa-trash-alt fa-lg"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>01 Jan 2045</td>
+                            <td>INV-0123</td>
+                            <td>Jhon Doe</td>
+                            <td>$123</td>
+                            <td class="d-flex align-items-lg-center justify-content-around">
+                                <a href="" class=""><i class="fas fa-eye fa-lg"></i></a>
+                                <a href="" class=""><i class="fas fa-user-edit fa-lg"></i></a>
+                                <a href="" class=""><i class="fas fa-trash-alt fa-lg"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>01 Jan 2045</td>
+                            <td>INV-0123</td>
+                            <td>Jhon Doe</td>
+                            <td>$123</td>
+                            <td class="d-flex align-items-lg-center justify-content-around">
+                                <a href="" class=""><i class="fas fa-eye fa-lg"></i></a>
+                                <a href="" class=""><i class="fas fa-user-edit fa-lg"></i></a>
+                                <a href="" class=""><i class="fas fa-trash-alt fa-lg"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>01 Jan 2045</td>
+                            <td>INV-0123</td>
+                            <td>Jhon Doe</td>
+                            <td>$123</td>
+                            <td class="d-flex align-items-lg-center justify-content-around">
+                                <a href="" class=""><i class="fas fa-eye fa-lg"></i></a>
+                                <a href="" class=""><i class="fas fa-user-edit fa-lg"></i></a>
+                                <a href="" class=""><i class="fas fa-trash-alt fa-lg"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>01 Jan 2045</td>
+                            <td>INV-0123</td>
+                            <td>Jhon Doe</td>
+                            <td>$123</td>
+                            <td class="d-flex align-items-lg-center justify-content-around">
+                                <a href="" class=""><i class="fas fa-eye fa-lg"></i></a>
+                                <a href="" class=""><i class="fas fa-user-edit fa-lg"></i></a>
+                                <a href="" class=""><i class="fas fa-trash-alt fa-lg"></i></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
+
+
     </div>
-    <!-- Blank End -->
+    <!-- data Subject table End -->
 </div>
 <!-- Content End -->
 
