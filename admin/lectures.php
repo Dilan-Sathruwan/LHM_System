8748276a719e7dd3d532e0cd7078d47538a55c1e
Ravindu lasal <ravindusl01@gmail.com>
2024-09-21 01:13:38 +0530

    test and create form

--- admin/lectures.php
+++ admin/lectures.php
@@ -7,6 +7,124 @@
     <!-- Navbar End -->
 
 
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Create Lecture profile</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- type here -->
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="justify-content-md-center">
+
+
+                                <div class="card my-1">
+
+                                    <form class="card-body cardbody-color p-lg-2">
+
+                                        <div class="row g-3">
+
+                                            <div class="text-center">
+                                                <img src="https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-1295397__340.png"
+                                                    class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                                                    width="200px" alt="profile">
+                                            </div>
+
+                                            <div class="col-sm-6">
+                                                <label for="firstName" class="form-label">Lecture Index Number</label>
+                                                <input type="text" class="form-control" id="Lecture Name" placeholder=""
+                                                    value="" required>
+                                                <div class="invalid-feedback">
+                                                    Valid Lecture name is required.
+                                                </div>
+                                            </div>
+
+                                            <div class="col-sm-6">
+                                                <label for="password" class="form-label">Password</label>
+                                                <input type="password" class="form-control" id="inputPassword3">
+                                                <div class="invalid-feedback">
+                                                    Valid password is required.
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12">
+                                                <label for="username" class="form-label">Username</label>
+                                                <div class="input-group has-validation">
+                                                    <input type="text" class="form-control" id="username"
+                                                        placeholder="Username" required>
+                                                    <div class="invalid-feedback">
+                                                        Your username is required.
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12">
+                                                <label for="email" class="form-label">Email </label>
+                                                <input type="email" class="form-control" id="email"
+                                                    placeholder="you@example.com">
+                                                <div class="invalid-feedback">
+                                                    Please enter a valid email address.
+                                                </div>
+                                            </div>
+
+
+                                            <div class="col-sm-6">
+                                                <label for="number" class="form-label">Mobile Number</label>
+                                                <input type="number" class="form-control" id="inputnumber">
+                                                <div class="invalid-feedback">
+                                                    Valid Mobile Number is required.
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12">
+                                                <label for="address" class="form-label">Address</label>
+                                                <input type="text" class="form-control" id="address"
+                                                    placeholder="1234, Main St,kegalle" required>
+                                                <div class="invalid-feedback">
+                                                    Please enter your address.
+                                                </div>
+                                            </div>
+
+                                            <div class="col-sm-6">
+                                                <label for="Lecturerole" class="form-label">Lecture Roles</label>
+                                                <select class="form-select" id="lecturerole" required>
+                                                    <option value="">Choose...</option>
+                                                    <option>Part time Lecture</option>
+                                                    <option>Visiting Lecture</option>
+                                                    <option>Permernet Lecture</option>
+                                                </select>
+                                                <div class="invalid-feedback">
+                                                    Please select a valid Roles.
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <button type="submit" class="btn btn-primary">Sign in</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
     <!-- data lectures table Start -->
     <div class="container-fluid pt-4 px-4">
 
@@ -18,7 +136,16 @@
 
                         <div class="card">
                             <div class="card-body table-responsive">
-                                <h5 class="card-title">Lectures Datatables</h5>
+                                <div class="d-flex align-items-center justify-content-between mb-4">
+                                    <h3 class="mb-0">Lectures Datatable</h3>
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                        data-bs-target="#staticBackdrop">
+                                        Add Lecture
+                                    </button>
+                                </div>
+
+                                <hr>
+
                                 <!-- Table with stripped rows -->
                                 <table class="table datatable text-start align-middle table-bordered table-hover mb-0">
                                     <thead>
@@ -94,6 +221,9 @@
                                                 <a href="" class=""><i class="fas fa-trash-alt fa-lg"></i></a>
                                             </td>
                                         </tr>
+
+
+
 
                                     </tbody>
                                 </table>
