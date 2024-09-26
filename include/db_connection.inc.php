082021891886b3efffcaa659eb4cbc71a55bcf1e
Tharuka-Marasinghe <tharukathineth27@gmail.com>
2024-09-26 13:46:05 +0530

    DB test

---
+++ include/db_connection.inc.php
@@ -0,0 +1,16 @@
+<?php
+// Database configuration
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "lecture_management_system";
+
+try {
+    // Create a PDO connection
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
