4d5d52a111b9e13c91e9d75a6b350f06f032b448
Shehani Christina <shehanichristinajayaweera@gmail.com>
2024-10-15 01:02:41 +0530

    Add data to the relevant tables

--- shehani/gallery.php
+++ shehani/gallery.php
@@ -129,6 +129,175 @@ Course Duration: 2 ½ years</p>
         </div>
     </div>
 
+    <!-- 2 nd yr Semester subjects-->
+    <div class="card mt-4 bg-dark border-light">
+            <div class="card-header text-center">
+              2 nd Year  1st Semester Subjects
+</div>
+<table class="table table-dark table-bordered text-center">
+  <thead>
+    <tr>
+                        <th>#</th>
+                        <th>Subject</th>
+                        <th>Lecturer</th>
+</tr>
+</thead>
+<tbody>
+  <tr>
+                         <td>1</td>
+                        <td>(HNDIT 1036) Computer Network</td>
+                        <td>Ms.l.k.Nikini</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <th>(HNDIT 1052) Web Programming</th>
+                        <th>Mr.R.K.D.Gamlath</th>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <th>(HNDIT 1042) System Analysis</th>
+                         <th>Ms.R.P.Anoosha </th>
+                    </tr>
+                    <tr>
+                         <td>4</td>
+                         <th>(HNDIT 1032) Algorithm </th>
+                         <th>Ms.s.k.Shamly</th>
+                    </tr>
+                    <tr>
+                          <td>5</td>
+                          <th>(HNDIT 1063) Technical Writing</th>
+                          <th>T.T.M.Kularathne</th>
+
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div>
+        <!--Second Yr 2nd Semester-->
+        <div class="card mt-4 bg-dark border-light">
+            <div class="card-header text-center">
+                 1st Year Semester 1 Subjects
+            </div>
+            <table class="table table-dark table-bordered text-center">
+                <thead>
+                <tr>
+                        <th>#</th>
+                        <th>Subject</th>
+                        <th>Lecturer</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>(HNDIT 3032) Data Structure And Algorithm</td>
+                        <td>Ms.W.M.D.Wepathana</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <th>(HNDIT 3012) Object Oriented Programming</th>
+                        <th>Mr.R.K.D.Gamlath</th>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <th>(HNDIT 3052) Operating System</th>
+                         <th>Ms.R.P.Anoosha </th>
+                    </tr>
+                    <td>4</td>
+                    <th>(HNDIT 3022) Web Programming</th>
+                    <th>Mr.R.K.D.Gamlath</th>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <th>(HNDIT 3062) Information and Computer Security</th>
+                    <th>Ms.K.l.K.K.Rathnasiri</th>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <th>(HNDIT 3072) Statics For It</th>
+                    <th>Ms.K.L.M.Udani</th>
+                </tr>
+
+
+
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <!-- 2 nd yr Semester subjects-->
+    <div class="card mt-4 bg-dark border-light">
+            <div class="card-header text-center">
+              2 nd Year  1st Semester Subjects
+</div>
+<table class="table table-dark table-bordered text-center">
+  <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>Subject</th>
+                        <th>Lecturer</th>
+                        </tr>
+</thead>
+                        <tbody>
+                    <tr>
+                         <td>1</td>
+                        <td>(HNDIT 23012) Operating Systems and Computer Security</td>
+                        <td>Ms.l.k.Nikini</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <th>(HNDIT 23093) Object Oriented Analysis and Design</th>
+                        <th><Mr class="T D Perera"></Mr></th>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <th>(HNDIT 24123) Software Configuration Management</th>
+                         <th>Ms.R.P.Weerakkodi </th>
+                    </tr>
+                    <tr>
+                         <td>4</td>
+                         <th>(HNDIT24143) Web Programming </th>
+                         <th>Ms.s.k.Shamly</th>
+                    </tr>
+                    <tr>
+                          <td>5</td>
+                          <th>(HNDIT 2309) Principals of Software Engineering</th>
+                          <th>T.Y.M.Yaparathne</th>
+
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div>
+
+ 
+                    
+
+
+
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
